845b6fd4efaa6dbfad7f5b65171bd41fc734a6b4
Jerin Sirija <jerinsirija+github@protonmail.com>
2025-01-25 15:53:58 +0530

    Add files via upload

---
+++ Simple Dictionary/app/src/main/java/np/com/jerin/simpledictionary/DictionaryApi.kt
@@ -0,0 +1,12 @@
+package np.com.jerin.simpledictionary
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryApi {
+
+    @GET("en/{word}")
+    suspend fun getMeaning(@Path("word") word : String) : Response<List<WordResult>>
+
+}
